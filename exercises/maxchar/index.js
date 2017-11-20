392cb1a890e92986def2a60aa6e3d47abde5e386
Evgeniy Ivon <ivoneug@gmail.com>
2017-11-20 15:14:27 +0300

    added max character solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let store = {};
+    let max = { key: '', value: 0 };
+
+    for(let char of str) {
+        if (char in store) {
+            store[char]++;
+        } else {
+            store[char] = 1;
+        }
+
+        // evaluate maximum character right in main loop
+        if (max.value < store[char]) {
+            max.key = char;
+            max.value = store[char];
+        }
+
+        // optimisation, when we're get char repetiance value >= half the 
+        // string length - no need to process further - we've got
+        // our max character
+        if (max.value >= str.length/2) break;
+    }
+
+    return max.key;
+}
 
 module.exports = maxChar;
