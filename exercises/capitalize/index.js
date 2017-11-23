1d3351da907835dcb267fcb904bc1d5af5ad5938
Evgeniy Ivon <ivoneug@gmail.com>
2017-11-23 11:20:21 +0300

    implemented capitalize problem solutions

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,42 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    return solution3(str);
+}
+
+function solution3(str) {
+    let result = str[0].toUpperCase();
+
+    for(let idx = 1; idx < str.length; idx++) {
+        if (str[idx - 1] === ' ') {
+            result += str[idx].toUpperCase();
+        } else {
+            result += str[idx];
+        }
+    }
+
+     return result;
+}
+
+function solution2(str) {
+    const words = [];
+
+    for(let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+     return words.join(' ');
+}
+
+function solution1(str) {
+    let arr = str.split(' ');
+
+    arr = arr.map((item) => {
+        return item[0].toUpperCase() + item.slice(1);
+    });
+
+    return arr.join(' ');
+}
 
 module.exports = capitalize;
