252f3a3393f4e6d044db4f4334c6c3d49fca4174
Evgeniy Ivon <ivoneug@gmail.com>
2017-11-27 15:52:09 +0300

    added steps problem solutions

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,57 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    solution3(n);
+}
+
+function solution3(n, row = 0, string = '') {
+    if (row === n) return;
+
+    if (string.length === n) {
+        console.log(string);
+        solution3(n, row + 1);
+        return;
+    }
+
+    const add = string.length <= row ? '#' : ' ';
+    solution3(n, row, string + add);
+}
+
+function solution2(n) {
+    let constructTheString = (string, step) => {
+        if (string.length === n) return string;
+
+        if (string.length <= step) {
+            string += '#';
+        } else {
+            string += ' ';
+        }
+
+        return constructTheString(string, step);
+    };
+
+    for(let i = 0; i < n; i++) {
+        let string = constructTheString('', i, 0);
+
+        console.log(string);
+    }
+}
+
+function solution1(n) {
+    for(let i = 0; i < n; i++) {
+        let string = '';
+
+        for (let j = 0; j < n; j++) {
+            if (j <= i) {
+                string += '#';
+            } else {
+                string += ' ';
+            }
+        }
+
+        console.log(string);
+    }
+}
 
 module.exports = steps;
