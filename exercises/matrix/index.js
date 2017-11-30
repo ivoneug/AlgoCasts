3f2825746cf80bdedf232603b37bdbaa7ce575b3
Evgeniy Ivon <ivoneug@gmail.com>
2017-11-30 11:42:24 +0300

    implemented spiral matrix problem solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,54 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        result.push([]);
+        // no need to add empty values to subarray cuz in JS we can assign
+        // value to any index in empty array
+        // for (let j = 0; j < n; j++) {
+        //     result[i].push(0);
+        // }
+    }
+
+    let counter = 1;
+    let column = 0;
+    let columnEnd = n - 1;
+    let row = 0;
+    let rowEnd = n - 1;
+
+    while (column <= columnEnd && row <= rowEnd) {
+        for (let idx = column; idx <= columnEnd; idx++) {
+            result[row][idx] = counter;
+            counter++;
+        }
+
+        row++;
+
+        for (let idx = row; idx <= rowEnd; idx++) {
+            result[idx][columnEnd] = counter;
+            counter++;
+        }
+
+        columnEnd--;
+
+        for (let idx = columnEnd; idx >= column; idx--) {
+            result[rowEnd][idx] = counter;
+            counter++;
+        }
+
+        rowEnd--;
+
+        for (let idx = rowEnd; idx >= row; idx--) {
+            result[idx][column] = counter;
+            counter++;
+        }
+
+        column++;
+    }
+
+    return result;
+}
 
 module.exports = matrix;
