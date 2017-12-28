ffe0fb678b917786315b3daad231f8fd80f69cce
Evgeniy Ivon <ivoneug@gmail.com>
2017-12-28 09:52:50 +0300

    added queue implementations

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,59 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+/**
+ * Queue using simple linked list
+ */
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    add(value) {
+        if (!this.tail) {
+            this.tail = { value };
+            this.head = this.tail;
+
+            return;
+        }
+
+        this.tail.prev = { value };
+        this.tail = this.tail.prev;
+    }
+
+    remove() {
+        if (!this.head) return undefined;
+
+        const value = this.head.value;
+
+        if (this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.prev;
+        }
+
+        return value;
+    }
+}
+
+/**
+ * Queue using array
+ * (should be slower on big amounts of data because of using of unshift)
+ */
+class Queue_array {
+    constructor() {
+        this.data = [];
+    }
+
+    add(value) {
+        this.data.unshift(value);
+    }
+
+    remove() {
+        return this.data.pop();
+    }
+}
 
 module.exports = Queue;
