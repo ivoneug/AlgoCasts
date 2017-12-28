954249f32ca5c116a01aee0bce462cfe713ac514
Evgeniy Ivon <evgeniy.ivon@3d4medical.com>
2017-12-28 16:11:36 +0300

    added queue from stacks problem implementation

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,48 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this._stack = new Stack();
+        this._stackRev = new Stack();
+        
+        this._reversed = false;
+    }
+    
+    _reverseToAdd() {
+        if (!this._reversed) return;
+
+        while (this._stackRev.peek()) {
+            this._stack.push(this._stackRev.pop());
+        }
+        
+        this._reversed = false;
+    }
+    
+    _reverseToRemove() {
+        if (this._reversed) return;
+
+        while (this._stack.peek()) {
+            this._stackRev.push(this._stack.pop());
+        }
+        
+        this._reversed = true;
+    }
+    
+    add(value) {
+        this._reverseToAdd();
+        this._stack.push(value);
+    }
+    
+    remove() {
+        this._reverseToRemove();
+        return this._stackRev.pop();
+    }
+    
+    peek() {
+        this._reverseToRemove();
+        return this._stackRev.peek();
+    }
+}
 
 module.exports = Queue;
